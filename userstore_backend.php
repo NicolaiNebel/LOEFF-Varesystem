dcf6b0c9eeb69be57c5f5fc16f6514e2a785c40e
NicolaiNebel <nicolainebel@gmail.com>
2014-05-15 19:35:41 +0200

    Added functionality to add items to an order and to place the order.

---
+++ userstore_backend.php
@@ -0,0 +1,36 @@
+<?php
+
+//takes in an array with $pid => $quantity structure and places the orders in
+//the sqlite db.
+//
+//Assumes $_SESSION['uid'] is set.
+
+if (!isset($_SESSION['Orders'])) {
+    $_SESSION['Orders'] = array();
+}
+
+//Items are added as a Product-object and a quantity.
+function addItem($product, $quantity) {
+    $_SESSION['Orders'][] = array($product->pid, $quantity);
+}
+
+function placeOrder($orderArray) {
+    $sql = 'INSERT INTO Orders(uid, pid, quantity) ' .
+           'VALUES (:uid, :pid, :quantity)';
+
+    $stat = $_SESSION['PDO']->prepare($sql);
+    $stat->bindValue(':uid', $_SESSION['uid'], PDO::PARAM_INT);
+
+    $stat->bindParam(':pid', $pid);
+    $stat->bindParam(':quantity', $q);
+
+    //PHP manual! I choose you! Don't fail me now!
+    foreach ($orderArray as $pid => $q) {
+        if (!$stat->execute()) {
+            echo 'Failed to insert order for uid = ',
+                $_SESSION['uid'], 'and pid = ', $pid, '.';
+        }
+    }
+}
+
+?>
